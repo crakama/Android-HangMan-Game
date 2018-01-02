172392da5abee88d7a4b25edaeeb493b43d5a341
crakama <crakama89@gmail.com>
2018-01-02 23:22:37 +0100

    Initial

---
+++ app/src/main/java/com/crakama/hangmandroid/services/ServeInterface.java
@@ -0,0 +1,16 @@
+package com.crakama.hangmandroid.services;
+
+import java.io.IOException;
+import java.net.Socket;
+
+/**
+ * Created by kate on 02/01/2018.
+ */
+
+public interface ServeInterface {
+
+    void startGame(ConnectionHandler connectionHandler, Socket clientSocket) throws IOException, ClassNotFoundException;
+    void initializeGame(Socket clientSocket) throws IOException;
+
+    void playGame(ConnectionHandler connHandler) throws IOException, ClassNotFoundException;
+}
